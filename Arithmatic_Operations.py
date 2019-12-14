98f6af9d72698811693e0d71d3bd9455e59b29b5
Akshay <akshaymithari98@gmail.com>
2019-12-14 13:54:51 +0530

    Akshay :  python 1 Arithmatic_Operations.py

---
+++ Arithmatic_Operations.py
@@ -0,0 +1,22 @@
+print("Enter No :")
+var = input()
+print("Enter No :")
+var1 = input()
+print("Addition is : ", int(var)+int(var1))
+print("substraction is : ", int(var)-int(var1))
+print("Multiplication is : ", int(var)*int(var1))
+print("Division is : ", int(var)/int(var1))
+print("Modulas is : ", int(var)%int(var1))
+print("preIncrement is : ", ++int(var))
+print("preDecrement is : ", --int(var))
+
+
+
+
+
+
+
+
+
+
+
